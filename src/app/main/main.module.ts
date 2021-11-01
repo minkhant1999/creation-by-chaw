c43f3d066b0a6e12ca824a88ba0e3a53dac93d4b
minkhant <minkhantmgmg@mytel.com.mm>
2021-11-01 15:19:03 +0630

    Products

--- src/app/main/main.module.ts
+++ src/app/main/main.module.ts
@@ -5,10 +5,12 @@ import { DashboardComponent } from '../components/dashboard/dashboard.component'
 import { GuideComponent } from '../components/guide/guide.component';
 import { PlantdetailsComponent } from '../components/plants/plantdetails/plantdetails.component';
 import { PlantsComponent } from '../components/plants/plants.component';
+import { ProductsComponent } from '../components/products/products.component';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'plants', component: PlantsComponent },
+  { path: 'products', component: ProductsComponent },
   { path: 'guide', component: GuideComponent },
   { path: 'plantdetails/:name', component: PlantdetailsComponent }
 ];
@@ -19,6 +21,7 @@ const routes: Routes = [
     GuideComponent,
     PlantdetailsComponent,
     DashboardComponent,
+    ProductsComponent
   ],
   imports: [
     RouterModule.forChild(routes),
