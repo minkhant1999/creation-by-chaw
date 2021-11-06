3ef1153e26a45075b8dc51aee9c5f58395df7cb4
minkhant <minkhantmgmg@mytel.com.mm>
2021-11-06 11:15:35 +0630

    C

--- src/app/components/checkout/checkout.component.ts
+++ src/app/components/checkout/checkout.component.ts
@@ -10,9 +10,17 @@ import { Plant } from 'src/app/components/plants/plants.component';
 export class CheckoutComponent implements OnInit {
   carts: any[] = []
   cartOpen = ''
+  total = 0
   constructor(private cart: CartService) { }
 
   ngOnInit(): void {
+    this.cart.getCarts().subscribe(data => {
+      this.carts = data;
+      this.total = 0;
+      this.carts.forEach(product => {
+        this.total += product.price;
+      })
+    })
   }
   removeFromCart(product: Plant) {
     this.cart.removeProduct(product)
