887a4242667dc582cdc3f88d396fe9173d430ad7
minkhant <minkhantmgmg@mytel.com.mm>
2021-12-10 16:32:11 +0630

    tailwind wheee

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,14 +3,9 @@ const { guessProductionMode } = require("@ngneat/tailwind");
 process.env.TAILWIND_MODE = guessProductionMode() ? 'build' : 'watch';
 
 module.exports = {
-    prefix: '',
-    mode: 'jit',
-    purge: {
-        content: [
-            './src/**/*.{html,ts,css,scss,sass,less,styl}',
-        ]
-    },
-    darkMode: false, // or 'media' or 'class'
+    content: [
+        './src/**/*.{html,ts,css,scss,sass,less,styl}',
+    ],
     theme: {
         extend: {},
     },
